92d84c6239cad0a8267ecb92a9c25a7812328845
aacohen <arielaades@gmail.com>
2017-03-08 11:21:04 -0500

    stackview for add child VC created

--- TheChart/AddChildViewController.swift
+++ TheChart/AddChildViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class AddChildViewController: UIViewController {
+    
+    let childNameLabel = UILabel()
+    let childAgeLabel = UILabel()
+    let childImageLabel = UILabel()
+    let nameTextField = UITextField()
+    let ageTextField = UITextField()
+    
+    let submitButton = UIButton()
+    var viewsArray = [UIView()]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
         view.backgroundColor = UIColor.blue
+        setupView()
+        createStackView()
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +33,53 @@ class AddChildViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func setupView() {
+        
+        
+        childNameLabel.text = "Child Name:"
+        childNameLabel.backgroundColor = UIColor.orange
+        
+        viewsArray.append(childNameLabel)
+        
+        childAgeLabel.text = "Child Age:"
+        childAgeLabel.backgroundColor = UIColor.orange
+        viewsArray.append(childAgeLabel)
+  
+        
+        childImageLabel.text = "Child Image"
+        childImageLabel.backgroundColor = UIColor.orange
+        viewsArray.append(childImageLabel)
+        
+        nameTextField.placeholder = "Name"
+        nameTextField.textAlignment = .center
+        nameTextField.backgroundColor = UIColor.gray
+        viewsArray.append(nameTextField)
+        
+        ageTextField.placeholder = "Age"
+        ageTextField.textAlignment = .center
+        ageTextField.backgroundColor = UIColor.gray
+        viewsArray.append(ageTextField)
+        
+        submitButton.titleLabel?.text = "Submit"
+        submitButton.backgroundColor = UIColor.orange
+        viewsArray.append(submitButton)
+    }
+    
+    func createStackView() {
+        
+        let stackView = UIStackView(arrangedSubviews: viewsArray)
+//        stackView.axis = .vertical
+//        stackView.distribution = .fillEqually
+//        stackView.alignment = .fill
+//        stackView.spacing = 5
+        childNameLabel.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.10)
+        childNameLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.40)
+        nameTextField.leadingAnchor.constraint(equalTo: childNameLabel.trailingAnchor)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
 
     /*
     // MARK: - Navigation
